13cc334f7e11d4abfbc82f0719088fdba5e698b1
Felix <buub1309120@gmail.com>
2025-09-18 21:56:13 +0700

    Update ocsight to v0.7.7

--- Formula/ocsight.rb
+++ Formula/ocsight.rb
@@ -1,24 +1,24 @@
 class Ocsight < Formula
   desc "OpenCode ecosystem observability platform"
   homepage "https://github.com/heyhuynhgiabuu/ocsight"
-  version "0.7.6"
+  version "0.7.7"
   
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/heyhuynhgiabuu/ocsight/releases/download/v0.7.6/ocsight-darwin-arm64.zip"
+      url "https://github.com/heyhuynhgiabuu/ocsight/releases/download/v0.7.7/ocsight-darwin-arm64.zip"
       sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     else
-      url "https://github.com/heyhuynhgiabuu/ocsight/releases/download/v0.7.6/ocsight-darwin-x64.zip"
+      url "https://github.com/heyhuynhgiabuu/ocsight/releases/download/v0.7.7/ocsight-darwin-x64.zip"
       sha256 "51970e8ca7ab4bc0f60abe0b13912f77c9bbadf30c6209bdc06df2bf7d36e513"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/heyhuynhgiabuu/ocsight/releases/download/v0.7.6/ocsight-linux-arm64.zip"
+      url "https://github.com/heyhuynhgiabuu/ocsight/releases/download/v0.7.7/ocsight-linux-arm64.zip"
       sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     else
-      url "https://github.com/heyhuynhgiabuu/ocsight/releases/download/v0.7.6/ocsight-linux-x64.zip"
+      url "https://github.com/heyhuynhgiabuu/ocsight/releases/download/v0.7.7/ocsight-linux-x64.zip"
       sha256 "357e3d78afbbfc109f4259df6cb1e33ce2928820426729cf40e9a64a10cdda96"
     end
   end
